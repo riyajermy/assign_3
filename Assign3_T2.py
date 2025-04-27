673a45cdbd5fcc1a5da6a509f51f19681a6ac21e
riyajermy <riya.jermy@gmail.com>
2025-04-27 20:00:47 +0530

    Add files via upload

---
+++ Assign3_T2.py
@@ -0,0 +1,10 @@
+import math
+num = float(input("Enter a number: "))
+
+square_root = math.sqrt(num)
+natural_log = math.log(num)
+sine_value = math.sin(num)
+
+print(f"Square root: {square_root}")
+print(f"Logarithm: {natural_log}")
+print(f"Sine: {sine_value}")
